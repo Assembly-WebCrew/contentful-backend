c735a018fda4f5beeee6e6b1a10a560a029467cf
Joona Romppanen <joona@remod.fi>
2017-08-02 23:35:27 +0300

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 npm pack
 export PACKAGE_NAME=$(ls -t1 | grep *.tgz | head -n 1)
 rsync -aPe 'ssh -o StrictHostKeyChecking=no' ./${PACKAGE_NAME} ${TRAVIS_BRANCH}:/opt/contentful-backend/builds/${PACKAGE_NAME}
-ssh -o StrictHosyKeyChecking=no ${TRAVIS_BRANCH} mkdir -p /opt/contentful-backend/${TRAVIS_BRANCH}
+ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} mkdir -p /opt/contentful-backend/${TRAVIS_BRANCH}
 ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} tar -xzf /opt/contentful-backend/builds/${PACKAGE_NAME} -C /opt/contentful-backend/${TRAVIS_BRANCH} --strip 1
 ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} cd /opt/contentful-backend/${TRAVIS_BRANCH} && yarn install
 ssh -t -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} sudo systemctl restart cf-backend-${TRAVIS_BRANCH}
