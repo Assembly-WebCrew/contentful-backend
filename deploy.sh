cb62c2f74a3b1de95b2ef02f28e12d27ac3e129e
Joona Romppanen <joona@remod.fi>
2017-08-02 23:47:58 +0300

    Wrap yarn install to bash exec

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ export PACKAGE_NAME=$(ls -t1 | grep *.tgz | head -n 1)
 rsync -aPe 'ssh -o StrictHostKeyChecking=no' ./${PACKAGE_NAME} ${TRAVIS_BRANCH}:/opt/contentful-backend/builds/${PACKAGE_NAME}
 ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} mkdir -p /opt/contentful-backend/${TRAVIS_BRANCH}
 ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} tar -xzf /opt/contentful-backend/builds/${PACKAGE_NAME} -C /opt/contentful-backend/${TRAVIS_BRANCH} --strip 1
-ssh -t -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} cd /opt/contentful-backend/${TRAVIS_BRANCH} && yarn install
+ssh -t -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} bash -c "cd /opt/contentful-backend/${TRAVIS_BRANCH} && yarn install"
 ssh -t -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} sudo systemctl restart cf-backend-${TRAVIS_BRANCH}
