54bcbb58fdffd05e9b0bd067a606e708900ef4fa
Joona Romppanen <joona@remod.fi>
2017-08-02 23:06:06 +0300

    Silence rsync and ssh. Force pseudo-terminal for service restart. Ensure branch dist dir is created.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npm pack
 export PACKAGE_NAME=$(ls -t1 | grep *.tgz | head -n 1)
-rsync -avPe 'ssh -o StrictHostKeyChecking=no' ./${PACKAGE_NAME} ${TRAVIS_BRANCH}:/opt/contentful-backend/builds/${PACKAGE_NAME}
-ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} tar -xzf /opt/contentful-backend/builds/${PACKAGE_NAME} -C /opt/contentful-backend/${TRAVIS_BRANCH} --strip 1
-ssh -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} sudo systemctl restart cf-backend-${TRAVIS_BRANCH}
+rsync -aPe 'ssh -o StrictHostKeyChecking=no' ./${PACKAGE_NAME} ${TRAVIS_BRANCH}:/opt/contentful-backend/builds/${PACKAGE_NAME}
+ssh -t -q -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} tar -xzf /opt/contentful-backend/builds/${PACKAGE_NAME} -C /opt/contentful-backend/${TRAVIS_BRANCH} --strip 1
+ssh -t -q -o StrictHostKeyChecking=no ${TRAVIS_BRANCH} sudo systemctl restart cf-backend-${TRAVIS_BRANCH}
